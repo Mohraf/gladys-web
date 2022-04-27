e90a3d874839ba0ced1fb096a1190416abdb4734
mohraf <amosngok@gmail.com>
2022-04-27 17:19:01 +0300

    set security right to grid

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,13 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+
+// nav menu responsiveness
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".hidden-menu");
+const accessButton = document.querySelector(".early");
+
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+  accessButton.classList.toggle("active");
+})
